e641e87d91760d515590fef22d566bbfb8c3f3a4
Jayson H Bucy <jayson.h.b@gmail.com>
2017-03-06 09:13:55 -0600

    wip

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,9 @@
+require 'httparty'
+
 class Kele
+  include HTTParty
+
   def initialize(email, password)
+    response = self.class.post(session_url("sessions"), body: { "email": email, "password": password })
   end
 end
