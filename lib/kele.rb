3471811bbd98144633296a2ced0a77dc9595e748
Jayson H Bucy <jayson.h.b@gmail.com>
2017-03-07 12:59:12 -0600

    Checkpoint 6

--- lib/kele.rb
+++ lib/kele.rb
@@ -25,6 +25,27 @@ class Kele
     @mentor_availability = JSON.parse(response.body)
   end
 
+  def get_messages(page=nil)
+    if page == nil
+      response = self.class.get(api_url("message_threads"), headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get(api_url("message_threads?page=#{page}"), headers: { "authorization" => @auth_token })
+    end
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipient_id, token, subject, stripped_text)
+    response = self.class.post(api_url("messages"),
+    body: {
+      sender: sender,
+      recipient_id: recipient_id,
+      token: token,
+      subject: subject,
+      stripped_text: stripped_text
+    },
+    headers: { "authorization" => @auth_token })
+  end
+
   private
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
