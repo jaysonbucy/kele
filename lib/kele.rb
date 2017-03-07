491e7edbfcd3ebeb77f58b8fa3d2ab82899fd86b
Jayson H Bucy <jayson.h.b@gmail.com>
2017-03-07 10:41:46 -0600

    Checkpoint 4

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,7 +9,6 @@ class Kele
 
   def initialize(email, password)
     response = self.class.post(api_url("sessions"), body: { email: email, password: password })
-    binding.pry
     raise Unauthorized, "Invalid email or password." if response.code == 404
     @auth_token = response["auth_token"]
   end
@@ -17,6 +16,11 @@ class Kele
   def get_me
     response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
     @user_data = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
   end
 
   private
